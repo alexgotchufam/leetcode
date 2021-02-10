3b5b7ff083b51d9bacda643212d9a03973a57730
Thinh Nguyen <thinhntr@gmail.com>
2021-02-11 03:03:47 +0700

    Update solution(Accepted)

--- 790/790.py
+++ 790/790.py
@@ -1,30 +1,37 @@
 class Solution:
     MOD = 10 ** 9 + 7
+
     def __init__(self):
-        self.dp = []
+        self.dp = None
 
-    def numTilings(self, N: int) -> int:
-        if N == 0:
-            return 1
-        elif N < 3:
-            return N
+    def numTilings(self, N: int, remain: int = 0) -> int:
+        if self.dp is None:
+            self.dp = [[None] * 2 for _ in range(N + 1)]
 
-        if len(self.dp) < N:
-            self.dp = [None] * (N + 1)
-            self.dp[0] = 1
+        if self.dp[N][remain] is not None:
+            return self.dp[N][remain]
 
         total = 0
-        for i in range(1, 4):
-            n_i = self.dp[N - i]
-            if n_i is None:
-                n_i = self.numTilings(N - i)
-                self.dp[N - i] = n_i
-            total += n_i % Solution.MOD
 
-        return 1 + total
+        if N == 1:
+            total = 1
+        elif N == 2:
+            total = 2
+        elif remain == 0:
+            total += (
+                self.numTilings(N - 1, 0)
+                + self.numTilings(N - 2, 0)
+                + 2 * self.numTilings(N - 2, 1)
+            )
+        else:
+            total += self.numTilings(N - 1, 0) + self.numTilings(N - 1, 1)
+
+        total %= Solution.MOD
+        self.dp[N][remain] = total
+        return total
 
 
 if __name__ == "__main__":
-    solution = Solution()
-    for i in range(1, 100):
-        print(solution.numTilings(i))
+    for i in range(1, 6):
+        solution = Solution()
+        print(i, solution.numTilings(i))
